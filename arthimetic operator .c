045e2e246d4edf9a9b202a8cfd714a00e211d335
Vivek Yadav <151618891+VivekAtGitHubs@users.noreply.github.com>
2024-09-04 00:40:45 +0530

    arthimetic operator .c

---
+++ arthimetic operator .c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main ()
+{
+int a,b ;
+printf ( "\n enter two number ");
+scanf ("%d%d",&a,&b);
+printf ("\n addition=%d",a+b);
+printf ("\n subtraction=%d ",a-b);
+printf ("\n multiplication=%d",a*b);
+printf ("\n division=%d",a/b);
+printf ("\n modulus =%d",a%b);
+return 0;
+}
